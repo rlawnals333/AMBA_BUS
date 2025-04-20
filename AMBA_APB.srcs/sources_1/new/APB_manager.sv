d669c8848cf739e7f4d4531ed92261fa2585dda6
JOOMIN KIM <rlawnals333@naver.com>
2025-04-20 21:48:17 +0900

    Signed-off-by: JOOMIN KIM <rlawnals333@naver.com>
    ambabus apb

---
+++ AMBA_APB.srcs/sources_1/new/APB_manager.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/04/18 12:21:29
+// Design Name: 
+// Module Name: APB_manager
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module APB_manager(
+
+    );
+endmodule
